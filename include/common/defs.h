62da6797a8215784e4219312eac49ad0c3d4c833
liwei <liwei.cpp@gmail.com>
2026-04-14 09:10:31 -0700

    some small fix

--- include/common/defs.h
+++ include/common/defs.h
@@ -30,6 +30,6 @@ namespace IOv2
     
     struct stream_error : std::runtime_error
     {
-        using std::runtime_error::runtime_error;;
+        using std::runtime_error::runtime_error;
     };
 }
