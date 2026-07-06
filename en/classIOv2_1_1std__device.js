0a6c2f201b5edb6fffb0f56c5ad508f648ba5ee6
liwei-cpp <38450168+liwei-cpp@users.noreply.github.com>
2026-07-06 14:46:03 +0000

    Deploying to gh-pages from @ liwei-cpp/IOv2@a3d97555b133c44de957e4ffc782ba908ec9e370 🚀

--- en/classIOv2_1_1std__device.js
+++ en/classIOv2_1_1std__device.js
@@ -5,7 +5,7 @@ var classIOv2_1_1std__device =
     [ "std_device", "classIOv2_1_1std__device.html#a8b54c0dcc980d1124b94a78e9ba28761", null ],
     [ "std_device", "classIOv2_1_1std__device.html#a3c0ba38d9bfda97a4a33d36840d23911", null ],
     [ "~std_device", "classIOv2_1_1std__device.html#a0cc53231f91e408343bebd4a01f7f72f", null ],
-    [ "deof", "classIOv2_1_1std__device.html#a2cf2418ae59fce7ee27cc16f52b52f6b", null ],
+    [ "deof", "classIOv2_1_1std__device.html#aadd0d1898ab983d6e4c13cdf79c2fcac", null ],
     [ "dflush", "classIOv2_1_1std__device.html#a9bbe7aeeef021bf8b68e1a96368b45d2", null ],
     [ "dget", "classIOv2_1_1std__device.html#aafa356f0924e3267a0e6aa834caac8cc", null ],
     [ "dput", "classIOv2_1_1std__device.html#a6ee8984169104a16908d459b46a5bc24", null ],
