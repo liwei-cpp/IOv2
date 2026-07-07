4e9701ad8236eeff512a9ebe2befa5d88b1be09a
liwei-cpp <38450168+liwei-cpp@users.noreply.github.com>
2026-07-07 17:38:21 +0000

    Deploying to gh-pages from @ liwei-cpp/IOv2@40414ef74ca5cecdec77a5b33d80b2e4f0729e5e 🚀

--- zh/classIOv2_1_1ostreambuf__iterator.js
+++ zh/classIOv2_1_1ostreambuf__iterator.js
@@ -1,6 +1,6 @@
 var classIOv2_1_1ostreambuf__iterator =
 [
-    [ "difference_type", "classIOv2_1_1ostreambuf__iterator.html#ab2211511dec8c411a7b8810fec888f00", null ],
+    [ "difference_type", "classIOv2_1_1ostreambuf__iterator.html#adcd2d57b191453b192d6e4402f64587a", null ],
     [ "value_type", "classIOv2_1_1ostreambuf__iterator.html#a08bc1ca92f96368eaa992a41308d39a0", null ],
     [ "ostreambuf_iterator", "classIOv2_1_1ostreambuf__iterator.html#ac1c21d569f9ca2a426fac61e31be7921", null ],
     [ "operator*", "classIOv2_1_1ostreambuf__iterator.html#a79f92b6f1d3ed0b1cda127b5c30d3fe6", null ],
