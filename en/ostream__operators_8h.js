fc0f8fd0f55895e290121f52db1a64fbfaa52b6c
liwei-cpp <38450168+liwei-cpp@users.noreply.github.com>
2026-07-19 01:46:10 +0000

    Deploying to gh-pages from @ liwei-cpp/IOv2@9de44ecc20f7f8c7e46d12ad48ef87a768eb8771 🚀

--- en/ostream__operators_8h.js
+++ en/ostream__operators_8h.js
@@ -8,10 +8,10 @@ var ostream__operators_8h =
     [ "IOv2::ostream_operators< TChar >", "structIOv2_1_1ostream__operators.html", "structIOv2_1_1ostream__operators" ],
     [ "IOv2::is_out_sentry", "conceptIOv2_1_1is__out__sentry.html", null ],
     [ "IOv2::ostream_type", "conceptIOv2_1_1ostream__type.html", null ],
+    [ "operator<<", "ostream__operators_8h.html#a37a665479733a4199ceb4d12f15edf34", null ],
+    [ "operator<<", "ostream__operators_8h.html#afd02f8624aa5da2d109f9fcdce62ac01", null ],
+    [ "operator<<", "ostream__operators_8h.html#a9e4d05c302e4f85eca99bff0ac11f385", null ],
     [ "operator<<", "ostream__operators_8h.html#a1224e46146283353583241d861425de9", null ],
-    [ "operator<<", "ostream__operators_8h.html#a1eafafebe3116a6af6ea77cfdb86f185", null ],
-    [ "operator<<", "ostream__operators_8h.html#a2e7e90db908268fef3a809eaf63ec934", null ],
-    [ "operator<<", "ostream__operators_8h.html#abb4dd45d5a8f145c262a77729df98bf0", null ],
     [ "operator<<", "ostream__operators_8h.html#a8cf627b4b038a6b901bab099239a6b72", null ],
     [ "operator<<", "ostream__operators_8h.html#a309ea80a8ac4e3f38a343955c054bd6a", null ],
     [ "operator<<", "ostream__operators_8h.html#a51198ad44e72fa662052cac879425b67", null ]
