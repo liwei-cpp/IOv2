548bfeb444589a1eb61a7de2bf042d4096a6ab28
liwei-cpp <38450168+liwei-cpp@users.noreply.github.com>
2026-07-26 23:19:10 +0000

    Deploying to gh-pages from @ liwei-cpp/IOv2@6e0b2cee75ffb7f509f86f7fdb00efdeb1cc16bb 🚀

--- en/io__manip_8h.js
+++ en/io__manip_8h.js
@@ -34,6 +34,6 @@ var io__manip_8h =
     [ "setbase", "io__manip_8h.html#a2d9efb4d2077d2b67b5cbc3c900b5e97", null ],
     [ "setfill", "io__manip_8h.html#a99ad8123323453cf572de3f0f942efee", null ],
     [ "setiosflags", "io__manip_8h.html#a907d98611f6b75d3b9e2b2b1cb534d8b", null ],
-    [ "setprecision", "io__manip_8h.html#a949e785951c135fb5ec1974003ae2677", null ],
-    [ "setw", "io__manip_8h.html#a5ea3dab8720430e1c4a6505ee483bd94", null ]
+    [ "setprecision", "io__manip_8h.html#a83ea5c0a2aaa3eed9f28feaa538facf4", null ],
+    [ "setw", "io__manip_8h.html#a37f9134c82e10179692acddd4624edfb", null ]
 ];
