ffe23142e189356875f22443e31fee4e8b674120
liwei-cpp <38450168+liwei-cpp@users.noreply.github.com>
2026-07-27 13:57:48 +0000

    Deploying to gh-pages from @ liwei-cpp/IOv2@9f2c6c4482e787884ccb98527b6c12f242322335 🚀

--- zh/structIOv2_1_1ostream__operators.js
+++ zh/structIOv2_1_1ostream__operators.js
@@ -1,6 +1,6 @@
 var structIOv2_1_1ostream__operators =
 [
-    [ "put", "structIOv2_1_1ostream__operators.html#aae8fda0762d505db0ada6009f40f955b", null ],
+    [ "put", "structIOv2_1_1ostream__operators.html#a9150e283c8b68aa30b9a706bccf98626", null ],
     [ "write", "structIOv2_1_1ostream__operators.html#a830aa88220a9ce16592ca1f200f16cff", null ],
     [ "operator<<", "structIOv2_1_1ostream__operators.html#a1808ef6ebeada0688ff7cfcb0530e171", null ]
 ];
